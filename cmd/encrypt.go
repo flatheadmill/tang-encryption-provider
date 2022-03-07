0305c43fbe0e39c48348c3f7fea355df46212a49
Alan Gutierrez <alan@prettyrobots.com>
2022-03-07 06:52:07 +0000

    Pass url and thumbprint as arguments.

--- cmd/encrypt.go
+++ cmd/encrypt.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"fmt"
+	"flag"
+	"os"
+
+	"io/ioutil"
 
 	"github.com/lainio/err2"
 	"github.com/bigeasy/tang-encryption-provider/crypter"
 )
 
-const Thumbprint = "o6U9qKv0_XdugefJV3q_NknYTY4Xgw27kcUnErkrVCY"
-
 func main() {
+	var (
+		url         = flag.String("url", "", "url of tang server")
+		thumbprint  = flag.String("thumbprint", "", "thumbprint of advertisement signing key")
+	)
+	flag.Parse();
 	input, err := ioutil.ReadAll(os.Stdin)
 	err2.Check(err)
-	encrypter, err := crypter.NewCrypter("http://tang:8080", Thumbprint)
+	encrypter, err := crypter.NewCrypter(*url, *thumbprint)
 	err2.Check(err)
 	compact, err := encrypter.Encrypt(input)
 	err2.Check(err)
