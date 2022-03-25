b869ee1d8aba7131f94e63a6b607a363fe1db76f
Alan Gutierrez <alan@prettyrobots.com>
2022-03-25 23:49:24 +0000

    Merge gRPC and directy test utilities.

--- cmd/decrypt.go
+++ cmd/decrypt.go
@@ -1,18 +1,69 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"net"
 	"os"
 
-	"io/ioutil"
-
+	"github.com/lainio/err2"
 	"github.com/lainio/err2/try"
+	"google.golang.org/grpc"
 
+	pb "k8s.io/apiserver/pkg/storage/value/encrypt/envelope/v1beta1"
 	crypter "github.com/flatheadmill/tang-encryption-provider/lestrrat"
 )
 
-func main() {
+func decryptWithKMS(socket string) (err error) {
+	defer err2.Return(&err)
+
+	input := try.To1(ioutil.ReadAll(os.Stdin))
+
+	dialer := func(ctx context.Context, addr string) (net.Conn, error) {
+		var d net.Dialer
+		return d.DialContext(ctx, "unix", addr)
+	}
+
+	conn := try.To1(grpc.Dial(socket, grpc.WithContextDialer(dialer), grpc.WithInsecure()))
+	defer conn.Close()
+
+	client := pb.NewKeyManagementServiceClient(conn)
+
+	ctx := context.Background()
+
+	version := try.To1(client.Version(ctx, &pb.VersionRequest{}))
+
+	fmt.Fprintf(os.Stderr, "%v\n", version)
+
+	plain := try.To1(client.Decrypt(ctx, &pb.DecryptRequest{Cipher: input}))
+
+	fmt.Printf("%v\n", string(plain.Plain))
+
+	return nil
+}
+
+func decryptWithTang() (err error) {
+	defer err2.Return(&err)
 	input := try.To1(ioutil.ReadAll(os.Stdin))
 	plain := try.To1(crypter.Decrypt(input))
 	fmt.Print(string(plain))
+	return nil
+}
+
+func main() {
+	var (
+		grpc = flag.String("grpc", "", "url of gRPC server")
+	)
+	flag.Parse()
+	var err error
+	if *grpc != "" {
+		err = decryptWithKMS(*grpc)
+	} else {
+		err = decryptWithTang()
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
 }
--- cmd/encrypt.go
+++ cmd/encrypt.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net"
 	"os"
 
-	"io/ioutil"
-
-	crypter "github.com/flatheadmill/tang-encryption-provider/lestrrat"
 	"github.com/lainio/err2"
 	"github.com/lainio/err2/try"
+	"google.golang.org/grpc"
+
+	pb "k8s.io/apiserver/pkg/storage/value/encrypt/envelope/v1beta1"
+	crypter "github.com/flatheadmill/tang-encryption-provider/lestrrat"
 )
 
-func run(url string, thumbprint string) (err error) {
+func decryptWithKMS(socket string) (err error) {
+	defer err2.Return(&err)
+
+	input := try.To1(ioutil.ReadAll(os.Stdin))
+
+	dialer := func(ctx context.Context, addr string) (net.Conn, error) {
+		var d net.Dialer
+		return d.DialContext(ctx, "unix", addr)
+	}
+
+	conn := try.To1(grpc.Dial(socket, grpc.WithContextDialer(dialer), grpc.WithInsecure()))
+	defer conn.Close()
+
+	client := pb.NewKeyManagementServiceClient(conn)
+
+	ctx := context.Background()
+
+	version := try.To1(client.Version(ctx, &pb.VersionRequest{}))
+
+	fmt.Fprintf(os.Stderr, "%v\n", version)
+
+	cipher := try.To1(client.Encrypt(ctx, &pb.EncryptRequest{Plain: input}))
+
+	fmt.Printf("%v\n", string(cipher.Cipher))
+
+	return nil
+}
+
+func decryptWithTang(url string, thumbprint string) (err error) {
 	err2.Return(&err)
 	input := try.To1(ioutil.ReadAll(os.Stdin))
 	encrypter := try.To1(crypter.NewCrypter(url, thumbprint))
@@ -23,11 +55,18 @@ func run(url string, thumbprint string) (err error) {
 
 func main() {
 	var (
-		url        = flag.String("url", "", "url of tang server")
+		grpc       = flag.String("grpc", "", "url of gRPC server")
+		tang       = flag.String("tang", "", "url of tang server")
 		thumbprint = flag.String("thumbprint", "", "thumbprint of advertisement signing key")
 	)
 	flag.Parse()
-	if err := run(*url, *thumbprint); err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+	var err error
+	if *grpc != "" {
+		err = decryptWithKMS(*grpc)
+	} else {
+		err = decryptWithTang(*tang, *thumbprint)
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, ">> %v\n", err)
 	}
 }
