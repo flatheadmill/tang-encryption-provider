d05a44d6c70ee81b63c208a7eee051c03c42c4ae
Jordan May <jordan.may@acretrader.com>
2022-04-28 10:18:31 -0500

    improve test_run.sh with auto thumbprint fetching and reduced redundancy.

--- cmd/test_run.sh
+++ cmd/test_run.sh
@@ -7,10 +7,10 @@ go build -o ../out/encrypt encrypt/encrypt.go
 go build -o ../out/decrypt decrypt/decrypt.go
 
 export TANG_KMS_SERVER_URL=http://localhost:8080
-export TANG_KMS_THUMBPRINT=QuUFrN0Ye4d31oEQ78j1eDHM6RXTu8hKr3Nk7tzLFdY
+export TANG_KMS_THUMBPRINT=$(curl -s $TANG_KMS_SERVER_URL/adv | jq -r '.payload' | base64 --decode | jq '.keys[0]' | jose jwk thp -i -)
 export TANG_KMS_UNIX_SOCKET=$HOME/dev/junk/socket
 
-mkdir -p $HOME/dev/junk/
+mkdir -p $(dirname $TANG_KMS_UNIX_SOCKET)
 touch $TANG_KMS_UNIX_SOCKET
 
 ../out/server &
@@ -19,5 +19,5 @@ sleep 1
 
 server=$!
 
-echo hello | ../out/encrypt -grpc $HOME/dev/junk/socket | ../out/decrypt -grpc $HOME/dev/junk/socket
+echo hello | ../out/encrypt -grpc $TANG_KMS_UNIX_SOCKET | ../out/decrypt -grpc $TANG_KMS_UNIX_SOCKET
 wait $server
